8e40d100a6f9fad322889d480395779f7f31a9d8
angelynaliem <angelynaliem@gmail.com>
2020-12-13 22:38:27 -0500

    Restricted middleware working

--- api/middleware/restricted-endpoint.js
+++ api/middleware/restricted-endpoint.js
@@ -1,36 +1,48 @@
 const jwt = require("jsonwebtoken");
-const secret = process.env.JWT_SECRET;
+const secrets = require("../../config/secrets.js");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization
-      ? req.headers.authorization.split(" ")[1]
-      : "";
-
-    if (token) {
-      jwt.verify(token, secret, (err, decodedToken) => {
-        if (err) {
-          next({ errCode: 401, errMessage: "Invalid credentials" });
-        } else {
-          req.decodedToken = decodedToken;
-          next();
-        }
-      });
-    } else {
-      next({ errCode: 401, errMessage: "Invalid credentials" });
-    }
+    const token = req.headers.authorization;
+    req.token = jwt.verify(token, secrets.jwtSecret);
+    console.log(req.token);
+    next();
   } catch (err) {
-    next({ errCode: 500, errMessage: "Server error validating credentials" });
+    console.log(err);
+    next({ errCode: 401, errMessage: "Invalid token" });
   }
-  /*
-    IMPLEMENT
-
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
 };
+
+// module.exports = (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization
+//       ? req.headers.authorization.split(" ")[1]
+//       : "";
+
+//     if (token) {
+//       jwt.verify(token, secret, (err, decodedToken) => {
+//         if (err) {
+//           next({ errCode: 401, errMessage: "Invalid credentials" });
+//         } else {
+//           req.decodedToken = decodedToken;
+//           next();
+//         }
+//       });
+//     } else {
+//       next({ errCode: 401, errMessage: "Invalid credentials" });
+//     }
+//   } catch (err) {
+//     next({ errCode: 500, errMessage: "Server error validating credentials" });
+//   }
+//   /*
+//     IMPLEMENT
+
+//     1- On valid token in the Authorization header, call next.
+
+//     2- On missing token in the Authorization header,
+//       the response body should include a string exactly as follows: "token required".
+
+//     3- On invalid or expired token in the Authorization header,
+//       the response body should include a string exactly as follows: "token invalid".
+//   */
+// };
